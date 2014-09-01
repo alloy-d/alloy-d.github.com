0a0c733efc4ad663a68cef6bccde6797e76e7386
Adam Lloyd <adam@alloy-d.net>
2014-09-01 15:37:21 -0400

    Recalculate parallaxy stuff on resize.

--- js/parallaxy-name.js
+++ js/parallaxy-name.js
@@ -1,43 +1,55 @@
 define(["domReady!"], function () {
   "use strict";
 
-  // As #name scrolls out of view, the .parallaxy-name things move down
-  // so that once #name is fully out of view, they are sitting at the top of the screen.
-  //
-  // At some point I should make this less shamefully single-purpose.
-  var repositionParallaxyThings = (function () {
-    var scrollingSection = document.querySelector("#name");
-    var scrollingSectionHeight = scrollingSection.offsetHeight;
-    var scrollingSectionShown = scrollingSectionHeight * 0.32; // FIXME: get a better number
-    var scrollingSectionHidden = scrollingSectionHeight - scrollingSectionShown;
+  var scrollingSection = document.querySelector("#name");
+  var affectedElements = document.querySelectorAll("h1.parallaxy-name");
 
-    var affectedElements = document.querySelectorAll("h1.parallaxy-name");
-    var initialOffset = parseInt(window.getComputedStyle(affectedElements[0]).top);
+  var repositionParallaxyThings;
 
-    var finalOffset = scrollingSectionShown*0.90;
-    var deltaOffset = finalOffset - initialOffset;
-    Array.prototype.forEach.call(affectedElements, function (elem) {
-      elem.style.position = "fixed";
-    });
+  var setUpParallaxyHandler = function () {
+    if (repositionParallaxyThings) {
+      window.removeEventListener("scroll", repositionParallaxyThings);
+    }
 
-    return function () {
-      var percentHidden, newOffset;
-      if (window.pageYOffset > scrollingSectionHidden) {
-        // No recalculation if the whole section has scrolled out of view.
-        Array.prototype.forEach.call(affectedElements, function (elem) {
-          elem.style.top = finalOffset + "px";
-        })
-        return;
-      }
+    // As #name scrolls out of view, the .parallaxy-name things move down
+    // so that once #name is fully out of view, they are sitting at the top of the screen.
+    //
+    // At some point I should make this less shamefully single-purpose.
+    repositionParallaxyThings = (function () {
+      var scrollingSectionHeight = scrollingSection.offsetHeight;
+      var scrollingSectionShown  = scrollingSectionHeight * 0.32; // FIXME: get a better number
+      var scrollingSectionHidden = scrollingSectionHeight - scrollingSectionShown;
 
-      percentHidden = window.pageYOffset/scrollingSectionHidden;
-      newOffset = initialOffset + percentHidden*deltaOffset + "px";
+      var initialOffset = parseInt(window.getComputedStyle(affectedElements[0]).top);
+
+      var finalOffset = scrollingSectionShown*0.90;
+      var deltaOffset = finalOffset - initialOffset;
       Array.prototype.forEach.call(affectedElements, function (elem) {
-        elem.style.top = newOffset;
+        elem.style.position = "fixed";
       });
-    };
-  }());
 
-  repositionParallaxyThings();
-  window.addEventListener("scroll", repositionParallaxyThings);
+      return function () {
+        var percentHidden, newOffset;
+        if (window.pageYOffset > scrollingSectionHidden) {
+          // No recalculation if the whole section has scrolled out of view.
+          Array.prototype.forEach.call(affectedElements, function (elem) {
+            elem.style.top = finalOffset + "px";
+          })
+          return;
+        }
+
+        percentHidden = window.pageYOffset/scrollingSectionHidden;
+        newOffset = initialOffset + percentHidden*deltaOffset + "px";
+        Array.prototype.forEach.call(affectedElements, function (elem) {
+          elem.style.top = newOffset;
+        });
+      };
+    }());
+
+    repositionParallaxyThings();
+    window.addEventListener("scroll", repositionParallaxyThings);
+  };
+
+  setUpParallaxyHandler();
+  window.addEventListener("resize", setUpParallaxyHandler);
 });
